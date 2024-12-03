f75f0aa47a704c239976e1b1e1745e20d8da5163
codebro <Samsondamilola.99@gmail.com>
2024-12-03 06:05:41 +0100

    added comma

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -7,12 +7,12 @@ export const verifyJWT = ({token}) => jwt.verify(token, process.env.JWT_SECRET);
 export const attachCookieToResponse = (res, {user}) =>{
     const generatedToken = createJWT({payload: user});
     const _5h = 1000*60*60*5;
-    res.cookie('token', generatedToken, {
+    res.cookie('token', generatedToken,  {
         httpOnly: true, 
         expires: new Date( Date.now() + _5h), 
         secure: false, 
         // secure: process.env.NODE_ENV === 'production', 
         signed: true, 
-        // sameSite: 'Strict'
+        // sameSite: 'Strict',
     })
 }
