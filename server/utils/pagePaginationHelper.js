aafa6f8de1993f9e6f7b7fe2579cd5c74a50c2c7
codebro <Samsondamilola.99@gmail.com>
2024-12-13 17:15:02 +0100

    change limit to 5 to test pagination

--- server/utils/pagePaginationHelper.js
+++ server/utils/pagePaginationHelper.js
@@ -1,6 +1,6 @@
 export const pagePaginationHelper = (page, limit) => {
     const pageNumber = parseInt(page) || 1;
-    const pageLimit = parseInt(limit) || 20;
+    const pageLimit = parseInt(limit) || 5;
     const skip = (pageNumber - 1) * pageLimit;
 
     return {limit, skip};
